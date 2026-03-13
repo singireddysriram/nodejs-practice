a242cd3f3d7a946390aac03a9a543a9b67c8aabc
singireddysriram <ssriram8790@gmail.com>
2026-03-13 23:09:08 +0530

    practice day 3

--- server.js
+++ server.js
@@ -1,35 +1,20 @@
-const http = require('http')
-const fs = require('fs')
-const path = require("path")
- 
- 
-const server = http.createServer((req,res)=>{
-    let filepath = "";
- 
-    if(req.url === "/"){
-        filepath = "index.html"
-    }else if(req.url ==="/about"){
-        filepath = "about.html"
-    }else{
-        filepath = "404.html"
-    }
- 
-    filepath = path.join(__dirname, filepath);
- 
-    fs.readFile(filepath,(err,data)=>{
-        if(err){
-            res.writeHead(500,{'content-type':'text/plain'})
-            res.end("error internal server ")
-        }else{
-             res.writeHead(200,{'content-type':'text/html'})
-            res.end(data)
-        }
-    });
-       
- 
-});
- 
-server.listen(3000,()=>{
-    console.log("server started ")
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect("mongodb+srv://ssriram8790:Sriram1234@cluster0.d3hujck.mongodb.net/?appName=Cluster0")
+.then(()=>{
+    console.log('mongo db connected...')
 })
 
+.catch((err)=>{
+    console.log(err)
+})
+app.get('/',(req,res)=>{
+    res.send("hi guys....")
+})
+
+ app.listen(4000,()=>{
+    console.log("server is running...")
+ })
