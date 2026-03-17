0b1c7af2cc9bc884e8843a9cfbc7645ffc93c45b
singireddysriram <ssriram8790@gmail.com>
2026-03-17 23:39:33 +0530

    practice day 3

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const Student = require('./models/studentModel')
-const Teacher = require('./models/teacherModel')
 
 const app = express()
 
-mongoose.connect("mongodb+srv://ssriram8790:Sriram1234@cluster0.d3hujck.mongodb.net/?appName=Cluster0")
+app.use(express.json())
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
  console.log("MongoDB connected")
 })
@@ -13,65 +16,98 @@ mongoose.connect("mongodb+srv://ssriram8790:Sriram1234@cluster0.d3hujck.mongodb.
  console.log(err)
 })
 
-//
-app.use(express.json())
 
-
-//home 
-app.get('/',(req,res)=>{
- res.send("hi guys....")
+// Home
+app.get("/",(req,res)=>{
+ res.send("Student CRUD API")
 })
 
 
-//newbee
-app.post("/student/api",async(req,res)=>{
+// CREATE
+app.post("/student/api", async(req,res)=>{
+ try{
+
+  const student = await Student.create(req.body)
+
+  res.status(201).json({
+   message:"Student created",
+   data:student
+  })
+
+ }catch(err){
+  res.status(500).json({error:err.message})
+ }
+})
+
+
+// READ ALL
+app.get("/students", async(req,res)=>{
+ try{
+
+  const students = await Student.find()
+
+  res.json({
+   message:"All students",
+   data:students
+  })
+
+ }catch(err){
+  res.status(500).json({error:err.message})
+ }
+})
+
+
+//read sort by id
+app.get("/students/api/:id",async(req,res)=>{
 try{
 
-    const student = await Student.create(req.body);
-     
-    res.status(201).json({
-        message: "student created is done",
-        data: student
+    const student = await Student.findById(req.params.id);
+
+    res.json({
+        message:"Single student",
+        data:student
     })
 
 }catch(err){
-res.status(500).json({
-    err: err.message
-})
+res.status(500).json({error:err.message})
 }
 })
 
-// teacher API
-app.post("/teacher/api", async (req, res) => {
-try {
+app.delete("/student/api/:id", async (req, res) => {
+ try {
 
-    const teacher = await Teacher.create(req.body);
+  const student = await Student.findByIdAndDelete(req.params.id);
 
-    res.status(201).json({
-        message: "teacher created successfully",
-        data: teacher
-    });
+  res.status(200).json({
+   message: "Student deleted",
+   data: student
+  });
 
-} catch (err) {
-
-    res.status(500).json({
-        err: err.message
-    });
-
-}
-});   
-
-
-app.get("/student", async(req, res)=>{
-    const student = await Student.find();
-    res.json(student)
-})
-
-app.get("/teacher", async (req, res) => {
-    const teacher = await Teacher.find();
-    res.json(teacher);
+ } catch (err) {
+  res.status(500).json({ error: err.message });
+ }
 });
 
+//update the data
+app.put("/students/api/:id",async(req,res)=>{
+try{
+const student = await Student.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {new:true}
+);
+
+res.json({
+    message:"updated done",
+    data:student
+})
+
+}catch(err){
+res.status(500).json({error:err.message})
+}
+})
+
+
 app.listen(4000,()=>{
- console.log("server running...")
+ console.log("Server running on port 4000")
 })
